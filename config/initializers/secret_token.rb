29fac8711eda0677a6c8545fe795cf1a05c39a27
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-05-18 13:49:46 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PublishingHackathon13::Application.config.secret_token = 'abaf31e5cf9572caf98ab364c2ba03137764fbf31184fe1a5d0b68b52c22991203c212d6bc2b084cd1d0d2ae288b8aac4da374097c5bc39a43820aa489aed96b'
